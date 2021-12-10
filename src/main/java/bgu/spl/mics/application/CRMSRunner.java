80cae3775ddc427258864da7883bddaf5d022d2f
Elias Assaf <elyas51000@gmail.com>
2021-12-10 19:38:51 +0200

    add gson, and try parsing

--- src/main/java/bgu/spl/mics/application/CRMSRunner.java
+++ src/main/java/bgu/spl/mics/application/CRMSRunner.java
@@ -1,11 +1,34 @@
 package bgu.spl.mics.application;
 
+
+
+
+
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /** This is the Main class of Compute Resources Management System application. You should parse the input file,
  * create the different instances of the objects, and run the system.
  * In the end, you should output a text file.
  */
 public class CRMSRunner {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Input input = new Input();
+        String path = args[0];
+        Gson gson = new Gson();
+        String tojson = gson.toJson(input);
+        try{
+            Reader reader = Files.newBufferedReader(Paths.get(path));
+            input = gson.fromJson(reader, Input.class);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        System.out.println("hi");
     }
 }
---
+++ src/main/java/bgu/spl/mics/application/Input.java
@@ -0,0 +1,20 @@
+package bgu.spl.mics.application;
+
+import bgu.spl.mics.application.objects.CPU;
+import bgu.spl.mics.application.objects.ConferenceInformation;
+import bgu.spl.mics.application.objects.GPU;
+import bgu.spl.mics.application.objects.Student;
+
+import java.util.List;
+
+public class Input {
+
+    List<Student> Students;
+    List<GPU> GPUS;
+    List<CPU> CPUS;
+    List<ConferenceInformation> Conferences;
+    int TickTime;
+    int Duration;
+
+
+}
