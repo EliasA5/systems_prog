da1658ed606aced1fa2a3403472dafee38119470
EliasA5 <elyas51000@gmail.com>
2021-12-27 17:24:50 +0200

    implement connections and connection handlers

---
+++ Server/src/main/java/bgu/spl/net/api/bidi/Connectionsimpl.java
@@ -0,0 +1,49 @@
+package bgu.spl.net.api.bidi;
+
+import bgu.spl.net.srv.bidi.ConnectionHandler;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Connectionsimpl<T> implements Connections<T>{
+
+    private ConcurrentHashMap<Integer, ConnectionHandler<T>> CHMap;
+    private AtomicInteger id;
+
+    public Connectionsimpl(){
+        CHMap = new ConcurrentHashMap<>();
+        id = new AtomicInteger(0);
+    }
+
+    @Override
+    public boolean send(int connectionId, T msg){
+        ConnectionHandler<T> handler = CHMap.get(connectionId);
+        if(handler == null)
+            return false;
+        handler.send(msg);
+        return true;
+    }
+
+    @Override
+    public void broadcast(T msg){
+        CHMap.forEachValue(1, (val) -> val.send(msg));
+    }
+
+    @Override
+    public void disconnect(int connectionId){
+        CHMap.remove(connectionId);
+    }
+
+    public int register(ConnectionHandler<T> handler){
+        int val;
+        do{
+            val = id.get();
+        }while(id.compareAndSet(val, val+1));
+        CHMap.put(val, handler);
+        return val;
+    }
+
+    public void clear(){ //used for testing
+        CHMap.clear();
+    }
+}
---
+++ Server/src/main/java/bgu/spl/net/srv/bidi/BlockingConnectionHandler.java
@@ -0,0 +1,72 @@
+package bgu.spl.net.srv.bidi;
+
+import bgu.spl.net.api.MessageEncoderDecoder;
+import bgu.spl.net.api.bidi.BidiMessagingProtocol;
+import bgu.spl.net.api.bidi.Connectionsimpl;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class BlockingConnectionHandler<T> implements Runnable,ConnectionHandler<T> {
+
+    private final BidiMessagingProtocol<T> protocol;
+    private final MessageEncoderDecoder<T> encdec;
+    private final Socket sock;
+    private BufferedInputStream in;
+    private BufferedOutputStream out;
+    private volatile boolean connected;
+
+
+
+    public BlockingConnectionHandler(Socket _sock, MessageEncoderDecoder _encdec, BidiMessagingProtocol _protocol){
+        sock = _sock;
+        protocol = _protocol;
+        encdec = _encdec;
+    }
+
+    @Override
+    public void send(T msg){
+        //TODO check if out != null is needed or connected == true
+        if(msg != null && connected)
+            try{
+                out.write(encdec.encode(msg));
+                out.flush();
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+    }
+
+    @Override
+    public void run(){
+        try(Socket sock = this.sock){
+            int read;
+            connected = true;
+            in = new BufferedInputStream(sock.getInputStream());
+            out = new BufferedOutputStream(sock.getOutputStream());
+
+            while (!protocol.shouldTerminate() && connected && (read = in.read()) >= 0) {
+                T nextMessage = encdec.decodeNextByte((byte) read);
+                if (nextMessage != null) {
+                    protocol.process(nextMessage);
+                }
+            }
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void close() throws IOException{
+        connected = false;
+        sock.close();
+    }
+
+    public void startProtocol(int connectionID, Connectionsimpl<T> connections){
+        protocol.start(connectionID, connections);
+    }
+
+}
