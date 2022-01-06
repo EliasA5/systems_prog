75e52a69e4a4c0aa00ae7a02d10829fdd4b8a780
EliasA5 <elyas51000@gmail.com>
2022-01-06 16:08:45 +0200

    compile for client

--- Client/include/helperFunctions.h
+++ Client/include/helperFunctions.h
@@ -8,20 +8,27 @@
 #include "boost/date_time/gregorian/gregorian.hpp"
 #include "boost/date_time/posix_time/posix_time.hpp"
 
-short bytesToShort(char* bytesArr)
+static short bytesToShort(char* bytesArr)
 {
     short result = (short)((bytesArr[0] & 0xff) << 8);
     result += (short)(bytesArr[1] & 0xff);
     return result;
 }
 
-void shortToBytes(short num, char* bytesArr)
+static short bytesToShort(const char* bytesArr)
+{
+    short result = (short)((bytesArr[0] & 0xff) << 8);
+    result += (short)(bytesArr[1] & 0xff);
+    return result;
+}
+
+static void shortToBytes(short num, char* bytesArr)
 {
     bytesArr[0] = ((num >> 8) & 0xFF);
     bytesArr[1] = (num & 0xFF);
 }
 
-std::string getDateAsString() {
+static std::string getDateAsString() {
     boost::posix_time::ptime time(boost::posix_time::second_clock::local_time());
     boost::gregorian::date::ymd_type ymd = time.date().year_month_day();
     std::stringstream dateStream;
